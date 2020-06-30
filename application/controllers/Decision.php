8870c03e691c177ee0df09eda95cd97e74811107
irchamali <id.irchamali@gmail.com>
2020-07-01 01:10:55 +0700

    Menambahkan fitur Dashboard dan Hasil Evaluasi untuk user Decision Maker

--- application/controllers/Decision.php
+++ application/controllers/Decision.php
@@ -8,6 +8,8 @@ class Decision extends CI_Controller
 	{
 		parent::__construct();
 		$this->load->model('Admin_model'); //load dulu modelnya agr bisa dipake semua method dalam satu controller
+		$this->load->model('Chart_model');
+		$this->load->model('Evaluation_model');
 		is_logged_in();
 		$this->load->library('form_validation');
 	}
@@ -16,6 +18,18 @@ class Decision extends CI_Controller
 	{
 		$data['title'] = 'Dashboard';
 		$data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
+
+		$data['jumAlt'] = $this->Admin_model->getDataAlternative();
+		$data['jumCrt'] = $this->Admin_model->getDataCriteria();
+		$data['jumSubCrt'] = $this->Admin_model->getDataSubcriteria();
+		$data['jumUser'] = $this->Admin_model->getDataUser();
+		//query-areachart
+		$data['graph'] = $this->Chart_model->getLabel();
+		$data['sumVal'] = $this->Chart_model->getSumVal();
+		//query-piechart
+		$data['dataPie'] = $this->Chart_model->getDataPie();
+		//query-barchart
+		$data['dataBar'] = $this->Chart_model->getDataBar();
 
 		$this->load->view('templates/header', $data);
 		$this->load->view('templates/sidebar', $data);
@@ -34,5 +48,21 @@ class Decision extends CI_Controller
 		$this->load->view('templates/topbar', $data);
 		$this->load->view('decision/dashboard', $data);
 		$this->load->view('templates/footer');
+	}
+
+	public function result()
+	{
+		$data['title'] = 'Hasil';
+		$data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
+
+		$data['getDataCriteria'] = $this->Evaluation_model->getDataCriteria();
+		$data['getDataAlternative'] = $this->Evaluation_model->getDataAlternative();
+		$data['sumVal'] = $this->Chart_model->getSumVal();
+
+		$this->load->view('templates/header', $data);
+		$this->load->view('templates/sidebar', $data);
+		$this->load->view('templates/topbar', $data);
+		$this->load->view('decision/result', $data);
+		$this->load->view('templates/footer2');
 	}
 }
--- application/views/decision/index.php
+++ application/views/decision/index.php
@@ -4,6 +4,190 @@
     <!-- Page Heading -->
     <h1 class="h3 mb-4 text-gray-800"><?= $title; ?></h1>
 
+    <?php if ($this->session->flashdata('message')) : ?>
+        <div class="row mt-3">
+            <div class="col-md-10">
+                <div class="alert alert-success alert-dismissible fade show" role="alert">
+                    Yeey, Data hasil evaluasi telah <strong>berhasil </strong><?= $this->session->flashdata('flash'); ?>terupdate!
+                    <button type="button" class="close" data-dismiss="alert" aria-label="close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+            </div>
+        </div>
+    <?php endif; ?>
+
+    <div class="row">
+        <!-- Earnings (Monthly) Card Example -->
+        <div class="col-xl-3 col-md-6 mb-4">
+            <div class="card border-left-primary shadow h-100 py-2">
+                <div class="card-body">
+                    <div class="row no-gutters align-items-center">
+                        <div class="col mr-2">
+                            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Alternative</div>
+                            <div class="row no-gutters align-items-center">
+                                <div class="col-auto">
+                                    <div class="h5 mb-0 mr-4 font-weight-bold text-gray-800"><?= $jumAlt;  ?></div>
+                                </div>
+                                <div class="col">
+                                    <div class="progress progress-sm mr-2">
+                                        <div class="progress-bar bg-primary" role="progressbar" style="width: <?= $jumAlt . '%'; ?>" aria-valuenow="<?= $jumAlt; ?>" aria-valuemin="0" aria-valuemax="100"></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-auto">
+                            <i class="fas fa-map fa-2x text-gray-300"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Earnings (Monthly) Card Example -->
+        <div class="col-xl-3 col-md-6 mb-4">
+            <div class="card border-left-success shadow h-100 py-2">
+                <div class="card-body">
+                    <div class="row no-gutters align-items-center">
+                        <div class="col mr-2">
+                            <div class="text-xs font-weight-bold text-success text-uppercase mb-1">Criteria</div>
+                            <div class="row no-gutters align-items-center">
+                                <div class="col-auto">
+                                    <div class="h5 mb-0 mr-4 font-weight-bold text-gray-800"><?= $jumCrt; ?></div>
+                                </div>
+                                <div class="col">
+                                    <div class="progress progress-sm mr-2">
+                                        <div class="progress-bar bg-success" role="progressbar" style="width: <?= $jumCrt . '%'; ?>" aria-valuenow="<?= $jumCrt; ?>" aria-valuemin="0" aria-valuemax="100"></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-auto">
+                            <i class="fas fa-wind fa-2x text-gray-300"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Earnings (Monthly) Card Example -->
+        <div class="col-xl-3 col-md-6 mb-4">
+            <div class="card border-left-info shadow h-100 py-2">
+                <div class="card-body">
+                    <div class="row no-gutters align-items-center">
+                        <div class="col mr-2">
+                            <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Subcriteria</div>
+                            <div class="row no-gutters align-items-center">
+                                <div class="col-auto">
+                                    <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800"><?= $jumSubCrt; ?></div>
+                                </div>
+                                <div class="col">
+                                    <div class="progress progress-sm mr-2">
+                                        <div class="progress-bar bg-info" role="progressbar" style="width: <?= $jumSubCrt . '%'; ?>" aria-valuenow="<?= $jumSubCrt; ?>" aria-valuemin="0" aria-valuemax="100"></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-auto">
+                            <i class="fas fa-clipboard-list fa-2x text-gray-300"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+        <!-- Pending Requests Card Example -->
+        <div class="col-xl-3 col-md-6 mb-4">
+            <div class="card border-left-warning shadow h-100 py-2">
+                <div class="card-body">
+                    <div class="row no-gutters align-items-center">
+                        <div class="col mr-2">
+                            <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">User</div>
+                            <div class="row no-gutters align-items-center">
+                                <div class="col-auto">
+                                    <div class="h5 mb-0 mr-4 font-weight-bold text-gray-800"><?= $jumUser; ?></div>
+                                </div>
+                                <div class="col">
+                                    <div class="progress progress-sm mr-2">
+                                        <div class="progress-bar bg-warning" role="progressbar" style="width: <?= $jumUser . '%'; ?>" aria-valuenow="<?= $jumUser; ?>" aria-valuemin="0" aria-valuemax="100"></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-auto">
+                            <i class="fas fa-user-edit fa-2x text-gray-300"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Content Row -->
+    <div class="row">
+
+        <div class="col-xl-8 col-lg-7">
+            <!-- Area Chart -->
+            <div class="card shadow mb-4">
+                <div class="card-header py-3">
+                    <h6 class="m-0 font-weight-bold text-primary">Area Chart</h6>
+                </div>
+                <div class="card-body">
+                    <div class="chart-area">
+                        <canvas id="areaChart"></canvas>
+                    </div>
+                    <hr>
+                    Grafik berbentuk <code>Area Chart</code> ini merupakan hasil dari perhitungan perbandingan berpasangan antara alternatif dengan masing-masing kriteria (x).
+                </div>
+            </div>
+
+            <!-- Bar Chart -->
+            <div class="card shadow mb-4">
+                <div class="card-header py-3">
+                    <h6 class="m-0 font-weight-bold text-primary">Bar Chart</h6>
+                </div>
+                <div class="card-body">
+                    <div class="chart-bar">
+                        <canvas id="barChart"></canvas>
+                    </div>
+                    <hr>
+                    Garafik berbentuk <code>Bar Chart</code> ini merupakan nilai (Ekl) yang didapat dari proses tahap akhir evaluasi kesesuaian lahan menggunakan metode Electre .
+                </div>
+            </div>
+
+        </div>
+
+        <!-- Donut Chart -->
+        <div class="col-xl-4 col-lg-5">
+            <div class="card shadow mb-4">
+                <!-- Card Header - Dropdown -->
+                <div class="card-header py-3">
+                    <h6 class="m-0 font-weight-bold text-primary">Donut Chart</h6>
+                </div>
+                <!-- Card Body -->
+                <div class="card-body">
+                    <div class="chart-pie pt-4">
+                        <canvas id="pieChart"></canvas>
+
+                    </div>
+                    <div class="mt-4 text-center small">
+                        <span class="mr-2">
+                            <i class="fas fa-circle text-primary"></i> S1
+                        </span>
+                        <span class="mr-2">
+                            <i class="fas fa-circle text-success"></i> S2
+                        </span>
+                        <span class="mr-2">
+                            <i class="fas fa-circle text-info"></i> S3
+                        </span>
+                    </div>
+                    <hr>
+                    Grafik berbentuk <code>Pie Chart</code> ini sebagai representasi dari pengelompokan alternatif dalam kelas kesesuaian lahan (S).
+                </div>
+            </div>
+        </div>
+    </div>
 
 
 </div>
